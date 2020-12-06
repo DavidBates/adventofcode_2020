b9526f4a337c0e292fa3ead2296aad4ccfcf71a1
David Bates <github@dlbates.com>
2020-12-06 01:59:41 -0500

    feat: day 5 part one

---
+++ day5/index.js
@@ -0,0 +1,42 @@
+const {match} = require('assert');
+const fs = require('fs');
+
+const parse_pass = (pass)=>{
+    const rows = 127
+    const cols = 8 
+
+    var row_array = Array.from(Array(rows), (x, index) => 0 + index);
+    var cols_array = Array.from(Array(cols), (x, index) => 0 + index);
+    pass.split('').map((letter)=>{
+  
+        switch(letter){
+            case 'B':
+                row_array = row_array.splice(Math.ceil(row_array.length / 2), row_array.length)
+                break;
+            case 'F':
+                row_array = row_array.splice(0, Math.ceil(row_array.length / 2))
+                break;
+            case 'L':
+                cols_array = cols_array.splice(0, Math.ceil(cols_array.length / 2))
+                break;
+            case 'R':
+                cols_array = cols_array.splice(Math.ceil(cols_array.length / 2), cols_array.length)
+                break;
+        }
+    })
+    return{pass:pass, row:row_array[0], col:cols_array[0], seatID:row_array[0]*8+cols_array[0]}
+    // debugging
+    // console.log(`${row_array[0]}, ${cols_array[0]}, ${row_array[0]*8+cols_array[0]}`)
+}
+
+var filename = process.argv[2];
+console.log(`reading values from ${filename}`)
+const bpasses = fs.readFileSync(filename).toString().split(/\n/).map((item)=>{
+    return parse_pass(item)
+})
+
+bpasses.sort((a,b)=>(a.seatID > b.seatID)? -1 : 1)
+console.log(`The highest boarding pass for part 1 is: ${bpasses[0].seatID}`)
+
+
+
