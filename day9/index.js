ecf88ef99c8e87c9dc7ef11a7bd2d5dff43a0a74
David Bates <github@dlbates.com>
2020-12-09 16:16:31 -0500

    feat: day 9 part two

--- day9/index.js
+++ day9/index.js
@@ -9,7 +9,7 @@ const numbers = fs.readFileSync(filename).toString().split(/\n/).map((number) =>
 })
 
 const preamble = 25
-
+let f_num = 0
 numbers.some((number, i) => {
  if(i > preamble){
      var c_num = numbers.slice(i - (preamble + 1), i)
@@ -22,8 +22,30 @@ numbers.some((number, i) => {
     // console.log(c_num)
     if(!c_num.length > 0){
         console.log(`The answer to part one is ${number}`)
+        f_num = number
         return true
     }
     return false
  }
 });
+
+// Part two
+var found = {}
+numbers.forEach((t_num, i)=>{
+    var acc = t_num
+    var f_i = i+1
+    var sub_arr = numbers.slice(i + 1, numbers.length - 1)
+    sub_arr.some((i_num, ii)=>{
+        acc = acc + i_num
+        f_i++
+        // console.log(acc)
+        return acc >= f_num
+    })
+    // console.log(`${acc}, ${f_num}`)
+    if(acc == f_num){
+        found = {start: i, end:f_i, sum: acc, arr: numbers.slice(i, f_i)}
+    }
+})
+const min = Math.min(...found.arr)
+const max = Math.max(...found.arr)
+console.log(`The answer to part two is: ${min+max}`)
